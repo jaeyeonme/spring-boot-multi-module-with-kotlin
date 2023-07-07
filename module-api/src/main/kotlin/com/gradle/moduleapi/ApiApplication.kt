1ed99ad3a6e91d64d43e25569276605283c04e07
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-07-07 17:22:31 +0900

    fix : fix api name

--- module-api/src/main/kotlin/com/gradle/moduleapi/ApiApplication.kt
+++ module-api/src/main/kotlin/com/gradle/moduleapi/ApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AplApplication
+class ApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<AplApplication>(*args)
+    runApplication<ApiApplication>(*args)
 }
